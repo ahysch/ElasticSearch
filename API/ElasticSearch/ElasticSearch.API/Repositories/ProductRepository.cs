a0ad935af2e531bb024031a41da107edce260a02
Ahmet Hakan Yıldız <70376278+ahysch@users.noreply.github.com>
2023-08-03 06:34:33 +0300

    nest

---
+++ API/ElasticSearch/ElasticSearch.API/Repositories/ProductRepository.cs
@@ -0,0 +1,104 @@
+﻿using ElasticSearch.API.DTOs;
+using ElasticSearch.API.Models;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Nest;
+using System.Collections.Immutable;
+
+namespace ElasticSearch.API.Repositories
+{
+    public class ProductRepository
+    {
+        private readonly ElasticClient _client;
+        private const string indexName = "productsaa";
+
+        public ProductRepository(ElasticClient client)
+        {
+            _client = client;
+        }
+
+        //public async Task<Product?> SaveAsync(Product newProduct)
+        //{
+        //    newProduct.Created = DateTime.Now;
+
+        //    var response = await _client.IndexAsync(newProduct, x => x.Index("products"));
+
+        //    if (!response.IsValid)
+        //    {
+        //        return null;
+        //    }
+
+        //    newProduct.Id = response.Id;
+
+        //    return newProduct;
+
+        //}
+
+
+        // Yukarıda id otomatik veriliyordu.Burada kendimiz guid atadık.
+        public async Task<Product?> SaveAsync(Product newProduct)
+        {
+            newProduct.Created = DateTime.Now;
+
+            var response = await _client.IndexAsync(newProduct, x => x.Index("products").Id(Guid.NewGuid().ToString()));
+
+            if (!response.IsValid)
+            {
+                return null;
+            }
+
+            newProduct.Id = response.Id;
+
+            return newProduct;
+
+        }
+        public async Task<ImmutableList<Product>> GetAllAsync()
+        {
+            var result = await _client.SearchAsync<Product>(
+                s=>s.Index(indexName)
+                    .Query(q=>q.MatchAll()));
+
+            foreach (var hit in result.Hits) 
+                hit.Source.Id = hit.Id;           
+                        
+            return result.Documents.ToImmutableList();
+        }
+        public async Task<Product?> GetByIdAsync(string id)
+        {
+            var response = await _client.GetAsync<Product>(id, x => x.Index(indexName));
+
+            if (!response.IsValid)
+            {
+                return null;
+            }
+
+            response.Source.Id = response.Id;
+            return response.Source;
+        }
+
+        public async Task<bool> UpdateAsync(ProductUpdateDto updateProduct) 
+        {
+            var response = await _client.UpdateAsync<Product, ProductUpdateDto>(updateProduct.Id, x => x.Index(indexName).Doc(updateProduct));
+
+            return response.IsValid;
+        }
+        /// <summary>
+        /// Hata yönetimi için bu method ele alınmıştır.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public async Task<DeleteResponse> DeleteAsync(string id)
+        {
+            var response = await _client.DeleteAsync<Product>(id, x => x.Index(indexName));
+
+            return response;
+        }
+
+        //Yukarıda metodu güncelledik.
+        //public async Task<bool> DeleteAsync(string id)
+        //{
+        //    var response = await _client.DeleteAsync<Product>(id, x => x.Index(indexName));
+
+        //    return response.IsValid;
+        //}
+    }
+}
---
+++ API/ElasticSearch/ElasticSearch.API/Services/ProductService.cs
@@ -0,0 +1,97 @@
+﻿using ElasticSearch.API.DTOs;
+using ElasticSearch.API.Models;
+using ElasticSearch.API.Repositories;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.Extensions.Logging;
+using Nest;
+using System.Collections.Immutable;
+using System.Net;
+
+namespace ElasticSearch.API.Services
+{
+    public class ProductService
+    {
+        private readonly ProductRepository _productRepository;
+        private readonly ILogger<ProductService> _logger;
+
+        public ProductService(ProductRepository productRepository, ILogger<ProductService> logger)
+        {
+            _productRepository = productRepository;
+            _logger = logger;
+        }
+
+        public async Task<ResponseDto<ProductDto>> SaveAsync(ProductCreateDto request)
+        {
+            var responseProduct = await _productRepository.SaveAsync(request.CreateProduct());
+            if (responseProduct == null)
+            {
+                return ResponseDto<ProductDto>.Fail(new List<string> { "Kayıt esnasında hata meydana geldi." }, HttpStatusCode.InternalServerError);
+            }
+
+            return ResponseDto<ProductDto>.Success(responseProduct.CreateDto(), HttpStatusCode.Created);
+        }
+
+        public async Task<ResponseDto<List<ProductDto>>> GetAllAsync()
+        {
+            var products = await _productRepository.GetAllAsync();
+            var productListDto = new List<ProductDto>();
+
+            foreach (var product in products) 
+            { 
+                var feature = product.Feature != null ? new ProductFeatureDto(product.Feature.Width,product.Feature.Height,product.Feature.Color.ToString()) : null;
+                productListDto.Add(new ProductDto(product.Id,product.Name,product.Price,product.Stock,feature));
+                
+            }
+            //var productListDto = products.Select(x=> new ProductDto(x.Id,x.Name,x.Price,x.Stock,new ProductFeatureDto(x.Feature.Width,x.Feature.Height,x.Feature.Color))).ToList();
+
+            return ResponseDto<List<ProductDto>>.Success(productListDto, HttpStatusCode.OK);
+        }
+        public async Task<ResponseDto<ProductDto>> GetByIdAsync(string id)
+        {
+            var hasProduct = await _productRepository.GetByIdAsync(id);
+
+            if (hasProduct == null)
+            {
+                return ResponseDto<ProductDto>.Fail("Ürün bulunamadı", HttpStatusCode.NotFound);
+            }
+
+            return ResponseDto<ProductDto>.Success(hasProduct.CreateDto(), HttpStatusCode.OK);
+        }
+        public async Task<ResponseDto<bool>> UpdateAsync(ProductUpdateDto updateProduct)
+        {
+            var isSuccess = await _productRepository.UpdateAsync(updateProduct);
+
+            if (!isSuccess)
+            {
+                return ResponseDto<bool>.Fail("Güncelleme esnasında bir hata meydana geldi",HttpStatusCode.InternalServerError);
+            }
+            return ResponseDto<bool>.Success(true, HttpStatusCode.NoContent);
+        }
+        //public async Task<ResponseDto<bool>> DeleteAsync(string id)
+        //{
+        //    var isSuccess = await _productRepository.DeleteAsync(id);
+
+        //    if (!isSuccess)
+        //    {
+        //        return ResponseDto<bool>.Fail("Silme esnasında bir hata meydana geldi", HttpStatusCode.InternalServerError);
+        //    }
+        //    return ResponseDto<bool>.Success(true, HttpStatusCode.NoContent);
+        //}
+
+        public async Task<ResponseDto<bool>> DeleteAsync(string id)
+        {
+            var deleteResponse = await _productRepository.DeleteAsync(id);
+
+            if (!deleteResponse.IsValid && deleteResponse.Result == Result.NotFound)
+            {
+                return ResponseDto<bool>.Fail("Silmek istediğiniz ürün bulunamadı.", HttpStatusCode.NotFound);
+            }
+            if (!deleteResponse.IsValid)
+            {
+                _logger.LogError(deleteResponse.OriginalException,deleteResponse.ServerError.Error.ToString());
+                return ResponseDto<bool>.Fail("Silme esnasında bir hata meydana geldi.", HttpStatusCode.InternalServerError);
+            }
+            return ResponseDto<bool>.Success(true, HttpStatusCode.NoContent);
+        }
+    }
+}
